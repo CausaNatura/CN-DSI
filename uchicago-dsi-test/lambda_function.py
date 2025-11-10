b5a3e43a45fbd2a486f6702146e02b6bd095d506
Jim Pivarski <jpivarski@gmail.com>
2025-11-10 13:49:53 -0600

    fix NameError and change key into a folder per day

--- uchicago-dsi-test/lambda_function.py
+++ uchicago-dsi-test/lambda_function.py
@@ -1,4 +1,6 @@
 import json
+from datetime import date
+
 import boto3
 
 SOCIAL_REGION = "us-east-1"
@@ -10,12 +12,12 @@ def lambda_handler(event, context):
     # parse these from the SNS payload
     media_id = "792642010493192"
     orig_phone_id = "phone-number-id-b43cdd61030c4f538a567f97822b2635"
-    key = "try3"
+    key = date.today().isoformat() + "/"
 
     resp = client.get_whatsapp_message_media(
         mediaId=media_id,
         originationPhoneNumberId=orig_phone_id,
-        destinationS3File={"bucketName": S3_bucket, "key": key},
+        destinationS3File={"bucketName": S3_BUCKET, "key": key},
     )
 
     return {"statusCode": 200, "body": resp}
