b26bff5d6a04061751694443ad821b8fa4f4dab9
Jim Pivarski <jpivarski@gmail.com>
2025-11-10 13:28:53 -0600

    try a hard-coded function first

---
+++ uchicago-dsi-test/lambda_function.py
@@ -0,0 +1,21 @@
+import json
+import boto3
+
+SOCIAL_REGION = "us-east-1"
+S3_BUCKET = "uchicago-causanatura-test"
+
+def lambda_handler(event, context):
+    client = boto3.client("socialmessaging", region_name=SOCIAL_REGION)
+
+    # parse these from the SNS payload
+    media_id = "792642010493192"
+    orig_phone_id = "phone-number-id-b43cdd61030c4f538a567f97822b2635"
+    key = "try3"
+
+    resp = client.get_whatsapp_message_media(
+        mediaId=media_id,
+        originationPhoneNumberId=orig_phone_id,
+        destinationS3File={"bucketName": S3_bucket, "key": key},
+    )
+
+    return {"statusCode": 200, "body": resp}
