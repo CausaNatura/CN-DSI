8f711de8a065494819386bf17df46483e757d7ff
Jim Pivarski <jpivarski@gmail.com>
2025-11-13 16:23:03 -0600

    developed everything but I have to unwire all the networking and start over

--- uchicago-dsi-test/lambda_function.py
+++ uchicago-dsi-test/lambda_function.py
@@ -1,22 +1,35 @@
+import base64
 import json
-import tempfile
 import os
-from datetime import date
+import sys
+import tempfile
+from datetime import datetime
 
 import boto3
+
+os.environ["PATH"] = (
+    "/mnt/deps/binary-dependencies/ffmpeg-git-20240629-amd64-static:"
+    + os.environ["PATH"]
+)
+sys.path.append("/mnt/deps/python-libraries")
+# import whisper
+import numpy as np
+
+# audio_to_text = whisper.load_model("medium", download_root="/mnt/deps/whisper-models")
 
 AWS_REGION = "us-east-1"
 S3_BUCKET = "uchicago-causanatura-test"
 
+socialmessaging = boto3.client("socialmessaging", region_name=AWS_REGION)
+s3 = boto3.client("s3", region_name=AWS_REGION)
+
 
 def lambda_handler(event, context):
-    socialmessaging = None
-    s3 = None
-
     for record in event.get("Records", []):
         sns_message = record.get("Sns", {})
         whatsapp_message = json.loads(sns_message.get("Message", ""))
         payload = json.loads(whatsapp_message.get("whatsAppWebhookEntry", "{}"))
+        whatsapp_message["whatsAppWebhookEntry"] = payload
 
         orig_phone_id = (
             whatsapp_message.get("context", {})
@@ -26,65 +39,63 @@ def lambda_handler(event, context):
             .replace("/", "-")
         )
 
-        print(f"Notification timestamp: {whatsapp_message.get('message_timestamp')}")
-
         for change in payload.get("changes", []):
             value = change.get("value", {})
-            print(f"Contacts: {json.dumps(value.get('contacts', []))}")
 
             for message in value.get("messages", []):
-                message_type = message.get("type")
-                print(f"Type: {message_type}")
+                try:
+                    timestamp = int(message.get("timestamp"))
+                except ValueError:
+                    continue
 
-                if message_type == "text":
-                    print(f"From: {message.get('from')}")
-                    print(f"Timestamp: {message.get('timestamp')}")
-                    print(f"Body: {message.get('text', {}).get('body')}")
+                sender = message.get("from")
 
-                elif message_type == "audio":
+                wamid = message.get("id", "")
+                if wamid.startswith("wamid."):
+                    wamid = wamid[6:]
+                short_id = (
+                    base64.b64encode(np.asarray(hash(base64.b64decode(wamid))))
+                    .decode()
+                    .rstrip("=")
+                )
+
+                s3_dir, output_filename = (
+                    datetime.fromtimestamp(timestamp).isoformat().split("T")
+                )
+                output_filename = (
+                    f"{sender}-{output_filename.replace(':', '-')}-{short_id}.json"
+                )
+
+                if message.get("type") == "audio":
                     audio = message["audio"]
-                    timestamp = message.get("timestamp")
                     media_type = audio.get("mime_type")
                     media_id = audio.get("id")
-                    try:
-                        s3_dir = date.fromtimestamp(int(timestamp)).isoformat()
-                    except ValueError:
-                        s3_dir = None
 
-                    print(f"From: {message.get('from')}")
-                    print(f"Timestamp: {timestamp}")
-                    print(f"Media-type: {media_type}")
-                    print(f"Media-id: {media_id}")
-
-                    if s3_dir is not None:
-                        if socialmessaging is None:
-                            socialmessaging = boto3.client(
-                                "socialmessaging", region_name=AWS_REGION
+                    result = socialmessaging.get_whatsapp_message_media(
+                        mediaId=media_id,
+                        originationPhoneNumberId=orig_phone_id,
+                        destinationS3File={
+                            "bucketName": S3_BUCKET,
+                            "key": f"{s3_dir}/",
+                        },
+                    )
+                    if result.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200:
+                        with tempfile.TemporaryDirectory() as td:
+                            ext_suffix = media_type.split(";")[0].split("/")[-1]
+                            s3_filename = f"{s3_dir}/{media_id}.{ext_suffix}"
+                            local_filename = os.path.join(
+                                td, f"{media_id}.{ext_suffix}"
                             )
+                            s3.download_file(S3_BUCKET, s3_filename, local_filename)
 
-                        result = socialmessaging.get_whatsapp_message_media(
-                            mediaId=media_id,
-                            originationPhoneNumberId=orig_phone_id,
-                            destinationS3File={
-                                "bucketName": S3_BUCKET,
-                                "key": f"{s3_dir}/",
-                            },
-                        )
+                            print(os.listdir(td))
 
-                        print(f"Result of copying to S3: {json.dumps(result)}")
+                            # message["transcription"] = audio_to_text.transcribe(local_filename)
 
-                        if result.get("ResponseMetadata", {}).get("HTTPStatusCode") == 200:
-                            if s3 is None:
-                                s3 = boto3.client("s3", region_name=AWS_REGION)
-
-                            with tempfile.TemporaryDirectory() as td:
-                                ext_suffix = media_type.split(";")[0].split("/")[-1]
-                                s3_filename = f"{s3_dir}/{media_id}.{ext_suffix}"
-                                local_filename = os.path.join(td, f"{media_id}.{ext_suffix}")
-                                s3.download_file(S3_BUCKET, s3_filename, local_filename)
-
-                                print(f"Downloaded: {os.stat(local_filename)}")
-
-
+                with tempfile.TemporaryDirectory() as td:
+                    full_filename = os.path.join(td, output_filename)
+                    with open(full_filename, "w") as output_file:
+                        json.dump(message, output_file)
+                    s3.upload_file(full_filename, S3_BUCKET, f"{s3_dir}/{output_filename}")
 
     return {"statusCode": 200}
